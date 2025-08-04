eaee2d978ae4c2d6b9f85d3c132c7811bf3e472d
HectorPalacios-R <ST10302442@vcconnect.edu.za>
2025-08-04 13:41:58 +0200

    Updated Program.cs

--- prjPROG6212_ICE_2/Program.cs
+++ prjPROG6212_ICE_2/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace prjPROG6212_ICE_2
+﻿using System.ComponentModel;
+
+namespace prjPROG6212_ICE_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] headings = { "Name", "Brand", "Inventory", "Price" };
+            Products product = new Products("Free Range Jumbo Eggs","Alzu Eggs", 200, 45.96);
+            Products product2 = new Products("Free Range Milk","Clover", 150, 35.50);
+            Console.WriteLine("Product Details:");
+            Console.WriteLine("Name: " + product["name"]);
+            Console.WriteLine("Brand: " + product["brand"]);
+            Console.WriteLine("Inventory: " + product["inventory"]);
+            Console.WriteLine("Price: R" + product["price"]);
+            product[2] = 490;
+            Console.WriteLine("Updated Inventory of Free Range Jumbo Eggs: " + product["inventory"]);
+            Console.WriteLine("\nProduct 2 Details:");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(headings[i] + (i == 3? ": R" : ": ") +product2[i]);
+            }
+            Console.ReadLine();
+           
+           
+
         }
     }
 }
